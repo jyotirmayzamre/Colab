1d9616de64c25652b5ff99c9c27dfeede09c5142
Joe <jyotirmayzamre@gmail.com>
2026-01-27 12:22:06 +0530

    Refactored redis_utils and consumers

--- backend/collaboration/consumers.py
+++ backend/collaboration/consumers.py
@@ -1,12 +1,13 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from .redis_utils import *
 from urllib.parse import parse_qs
+import asyncio
 
 
 class DocumentConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        self.doc_id = self.scope.get("url_route", {}).get("kwargs", {}).get("document_id")
-        self.group_name = f"document_{self.doc_id}"
+        self.document_id = self.scope.get("url_route", {}).get("kwargs", {}).get("document_id")
+        self.group_name = f"document_{self.document_id}"
 
         query_string = self.scope["query_string"].decode()
         params = parse_qs(query_string)
@@ -15,16 +16,16 @@ class DocumentConsumer(AsyncJsonWebsocketConsumer):
         await self.channel_layer.group_add(
             self.group_name, self.channel_name
         )
-        await redis_generate_colours(self.doc_id)
-        self.colour = await redis_assign_colour(self.doc_id)
+        await redis_generate_colours(self.document_id)
+        self.colour = await redis_assign_colour(self.document_id)
 
         
         await self.accept()
 
-        await redis_add_user(self.doc_id, self.channel_name)
-        current_count = await redis_get_user_count(self.doc_id)
+        await redis_add_user(self.document_id, self.channel_name)
+        current_count = await redis_get_user_count(self.document_id)
 
-        state, title = await redis_load_crdt(self.doc_id)
+        state, title = await redis_load_crdt(self.document_id)
         await self.send_json({'event': 'load.crdt', 'state': state, 'user_count': current_count, 'title': title})
 
         await self.channel_layer.group_send(
@@ -37,50 +38,52 @@ class DocumentConsumer(AsyncJsonWebsocketConsumer):
             self.group_name, self.channel_name
         )
 
-        await redis_add_colour(self.doc_id, self.colour)
+        await redis_add_colour(self.document_id, self.colour)
 
-        await redis_remove_user(self.doc_id, self.channel_name)
-        remaining_count = await redis_get_user_count(self.doc_id)
+        await redis_remove_user(self.document_id, self.channel_name)
+        remaining_count = await redis_get_user_count(self.document_id)
          
         if(remaining_count > 0):
             await self.channel_layer.group_send(
                 self.group_name, {'type': 'userCount.updated', 'sender': self.channel_name, 'user_count': remaining_count}
             )
         else:
-            await redis_remove_colours(self.doc_id)
-            await redis_flush_to_db(self.doc_id)
+            await redis_flush_to_db(self.document_id)
 
 
 
     async def receive_json(self, content):
-        if(content['type'] == 'char'):
-            await self.channel_layer.group_send(
-                self.group_name, {'type': 'crdt.oper', 'content': content, 'sender': self.channel_name}
-            )
-            await redis_update_crdt(self.doc_id, content)
+        match content['type']:
 
-        elif(content['type'] == 'version_restore'):
-            state = await redis_restore_version(content['versionId'])
+            case 'char':
+                await self.channel_layer.group_send(
+                    self.group_name, {'type': 'crdt.oper', 'content': content, 'sender': self.channel_name}
+                )
+                await redis_update_crdt(self.document_id, content)
+                
+            case 'version_restore':
+                state = await redis_restore_version(content['versionId'])
+                await self.channel_layer.group_send(
+                    self.group_name, {'type': 'version.restore', 'versionId': content['versionId'], 'state': state}
+                )
 
-            await self.channel_layer.group_send(
-                self.group_name, {'type': 'version.restore', 'versionId': content['versionId'], 'state': state}
-            )
+            case 'document_rename':
+                await redis_set_title(content['newTitle'], self.document_id)
+                await self.channel_layer.group_send(
+                    self.group_name, {'type': 'document.rename', 'newTitle': content['newTitle']}
+                )
 
-        elif(content['type'] == 'document_rename'):
-            await self.channel_layer.group_send(
-                self.group_name, {'type': 'document.rename', 'newTitle': content['newTitle']}
-            )
-
-        elif(content['type'] == 'cursor_update'):
-            await self.channel_layer.group_send(
-                self.group_name, {'type': 'cursor.update', 'sender': self.channel_name, 
-                                  'username': self.username, 'col': content['col'], 'row': content['row'],
-                                  'colour': self.colour}
-            )
-
-        else:
-            pass
-
+            case 'cursor_update':
+                await self.channel_layer.group_send(
+                self.group_name, {
+                    'type': 'cursor.update', 'sender': self.channel_name, 
+                    'username': self.username, 'col': content['col'], 'row': content['row'],
+                    'colour': self.colour
+                    })
+                
+            case _:
+                pass
+                
 
 
     async def version_restore(self, event):
--- backend/collaboration/redis_utils.py
+++ backend/collaboration/redis_utils.py
@@ -1,10 +1,12 @@
 import json
 import redis.asyncio as redis
+import asyncio
 from asgiref.sync import sync_to_async
 from documents.services import DocumentService
 from versions.services import VersionService
 from .crdt import remoteDelete, remoteInsert
 import colorsys
+from channels.db import database_sync_to_async
 
 # client: redis.Redis = redis.Redis(host='redis', port=6379, db=0)
 client: redis.Redis = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
@@ -12,17 +14,21 @@ client: redis.Redis = redis.Redis(host='localhost', port=6379, db=0, decode_resp
 '''
 Used for cursor labels
 '''
+    
 async def redis_generate_colours(document_id, n=200, saturation=0.65, value=0.95):
-    set_exists = await client.exists(f'colours:{document_id}')
-    if not set_exists:
-        colours = set()
-        for i in range(n):
-            hue = i / n
-            r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
-            colours.add(f"#{int(r*255):02X}{int(g*255):02X}{int(b*255):02X}")
+    if await client.exists(f'colours:{document_id}'):
+        return
+    
+    colours = set()
+    for i in range(n):
+        hue = i/n
+        r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
+        colours.add(f"#{int(r*255):02X}{int(g*255):02X}{int(b*255):02X}")
 
-        await client.sadd(f'colours:{document_id}', *colours) # type: ignore
 
+    await client.sadd(f'colours:{document_id}', *colours) # type: ignore
+
+    
 async def redis_assign_colour(document_id):
     colour = await client.spop(f'colours:{document_id}') #type: ignore
     return colour
@@ -32,58 +38,73 @@ async def redis_add_colour(document_id, colour):
     await client.sadd(f'colours:{document_id}', colour) #type: ignore
 
 
-async def redis_remove_colours(document_id):
-    await client.delete(f'colours:{document_id}')
-
-
-
+@database_sync_to_async
+def _get_document(document_id):
+    return DocumentService.get_document(document_id)
 
 async def redis_load_crdt(document_id):
-    state_json = await client.get(f'crdt:{document_id}')
-    title = await client.get(f'title:{document_id}')
+    state_json, title = await client.mget(
+        f'crdt:{document_id}',
+        f'title:{document_id}'
+    )
+   
     if state_json and title:
         return json.loads(state_json), title
     
-    doc = await sync_to_async(DocumentService.get_document)(document_id)
-    state = doc.state
-    title = doc.title
+    doc = await _get_document(document_id)
 
-    await client.set(
-        f'crdt:{document_id}',
-        json.dumps(state)
-    )
-
-    await client.set(
-        f'title:{document_id}',
-        title
-    )
-
+    await client.mset({
+        f'crdt:{document_id}': json.dumps(doc.state),
+        f'title:{document_id}': doc.title
+    })
     
-    return state, title
+    return doc.state, doc.title
     
 
-async def redis_update_crdt(document_id, content):
-    state_json = await client.get(f'crdt:{document_id}')
-    if state_json is None:
-        return
-    
-    state = json.loads(state_json)
-    
-    for operation in content['data']:
-        if operation['oper'] == 'Insert':
-            state = remoteInsert(operation['row'], operation['char'], state)
+def _apply_crdt_ops(state, operations):
+    for op in operations:
+        if op["oper"] == "Insert":
+            state = remoteInsert(op["row"], op["char"], state)
         else:
-            state = remoteDelete(operation['row'], operation['char'], state)
+            state = remoteDelete(op["row"], op["char"], state)
+    return state
 
-    await client.set(f'crdt:{document_id}', json.dumps(state))
+async def redis_update_crdt(document_id, content): 
+    key = f'crdt:{document_id}'
 
+    
+    while True:
+        try:
+            async with client.pipeline(transaction=True) as pipe:
+                await pipe.watch(key)
+                
+                state_json = await pipe.get(key)
+                
+                if state_json is None:
+                    await pipe.unwatch()
+                    return
+                
+                state = json.loads(state_json)
+                state = await asyncio.to_thread(
+                    _apply_crdt_ops, state, content["data"]
+                )
+                
+                pipe.multi()
+                pipe.set(key, json.dumps(state))
+                await pipe.execute()
+                return 
+            
+        except redis.WatchError:
+            await asyncio.sleep(0)
+        
+
+@database_sync_to_async
+def _get_version_state(version_id):
+    return VersionService.get_version_state(version_id)
 
 async def redis_restore_version(version_id):
-    state, document_id = await sync_to_async(VersionService.get_version_state)(version_id)
-    await client.set(
-        f'crdt:{document_id}',
-        json.dumps(state)
-    )
+    state, document_id = await _get_version_state(version_id)
+    await client.set(f'crdt:{document_id}',json.dumps(state))
     return state
 
 async def redis_add_user(document_id, user_id):
@@ -97,17 +118,28 @@ async def redis_get_user_count(document_id):
     return count
 
 
+@database_sync_to_async
+def _flush_document_state(document_id, state):
+    DocumentService.update_state(document_id, state)
+
 async def redis_flush_to_db(document_id):
-    state = await client.get(f'crdt:{document_id}')
-    if(state):
-        state_decoded = json.loads(state)
-        await sync_to_async(DocumentService.update_state)(
-            document_id=document_id,
-            state=state_decoded
-        )
-    await client.delete(f'crdt:{document_id}')
-    await client.delete(f'users:{document_id}')
-    await client.delete(f'colours:{document_id}')
+    state_json = await client.get(f'crdt:{document_id}')
 
-
+    if not state_json:
+        return
     
+    state = json.loads(state_json)
+
+    await _flush_document_state(document_id, state)
+
+    await client.delete(
+        f'crdt:{document_id}',
+        f'users:{document_id}',
+        f'colours:{document_id}',
+        f'title:{document_id}'
+    )
+    
+
+
+async def redis_set_title(title, document_id):
+    await client.set(f'title:{document_id}', title)
